a027623f09e8d79e0763f285ac4f16933742c0d9
Tomas <tg4263340@gmail.com>
2026-02-18 07:48:38 -0300

    Subo mi portafolio

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/skills')
+@app.route('/projects')
+@app.route('/about')
+def home():
+    return render_template('layout.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
